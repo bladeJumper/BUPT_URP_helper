1b855ed8f983255a60bc5625242b8a2b1d292db7
Xie Zhuojun <630302402@qq.com>
2018-07-10 20:25:49 +0800

    performance enhancement and function update

--- URP_helper.py
+++ URP_helper.py
@@ -1,14 +1,20 @@
 
-#北邮选课助手 version 1.0
 import requests
 import time
 from PIL import Image,ImageEnhance
 import pytesseract
 
-#登陆数据头部，在这里输入你的学号和密码，POST登陆请求时程序将自动在尾部加入验证码形成完整的POST数据
-dataheader = "type=sso&zjh=学号&mm=密码&v_yzm="
-host = "10.3.255.178"
 
+#登陆数据头部，在这里输入你的学号和密码，POST登陆请求时程序将自动在尾部加入验证码形成完整的POST数据
+dataheader = "type=sso&zjh=2016211287&mm=54yzwddsg&v_yzm="
+#教务系统主机ip
+host = "10.3.255.178"
+#选课信息
+kcid = ["3132121100_01"]
+kcty = ["2"]
+success = [0]
+maxloop = 0
+sleeptime= 0.1
 #验证码识别
 def image_to_str(path):
     image = Image.open(path)
@@ -90,10 +96,10 @@ def coursedata(kcid,kcty,cookies):
         "Accept-Encoding":"gzip, deflate",
         "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,zh-TW;q=0.5",
     }
-    page = 1
+    page = 2
     url = "http://10.3.255.178/xkAction.do?actionType="+kcty+"&pageNumber="+str(page)+"&oper1=ori"
     res = requests.get(url, headers = headers, cookies = cookies)
-
+    '''
     #检查是否找到课程
     chk = kcid[0:9]
     inchk = "课程"
@@ -114,8 +120,12 @@ def coursedata(kcid,kcty,cookies):
             return "-1"
         time.sleep(0.1)
     return "-1"
-
-
+    '''
+    chk = "2016"
+    if chk in res.text:
+        return 1
+    else:
+        return 0
 
 #选课中间函数,参数为课程id，课程种类（方案课程（2）、系任选课（4）、校任选课（3）），课程所在页数,cookies
 def xk(kcid,coursetype,page,cookies):
@@ -137,34 +147,17 @@ def xk(kcid,coursetype,page,cookies):
         "actionType":"9"}
 
     response = requests.post('http://10.3.255.178/xkAction.do', headers = headers, cookies = cookies, data = data)
-    faichk = "对不起"
     succchk = "成功"
-    if faichk in response.text:
-        return 0
-    elif succchk in response.text:
+    nologchk = "初始化函数"
+    if succchk in response.text:
         return 1
-    else:
+    elif nologchk in response.text:
         return -1
+    else:
+        return 0
 
-#尝试选课，成功则return 1，达到最大尝试次数则return 0
-def attempt(kcid,coursetype,page,cookies,maxloop,sleeptime):
-    loopcount = 1
-    while(loopcount <= maxloop):
-        succhk = xk(kcid,coursetype,page,cookies)
-        print("第",loopcount,"次尝试于 ",time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-        if(succhk == 1):
-            print("选课成功！")
-            return 1
-        elif(succhk == 0):
-            print("课程时间冲突或已达到最大可选课程数！")
-        else:
-            print("其他错误")
-        loopcount = loopcount + 1
-        time.sleep(sleeptime)
-        if (loopcount > maxloop):
-            return 0
-
-#课程搜索，若没有找到且选择放弃选课，则return -1，其他情况return正整数，默认return 1
+#课程输入，若没有找到且选择放弃选课，则return -1，其他情况return正整数，默认return 1
+#已不需要，无需搜索到课程也可以选课
 def search(kcid,kcty,cookies):
     pages = coursedata(kcid,kcty,cookies)
     while(pages == '-1'):
@@ -183,7 +176,6 @@ def search(kcid,kcty,cookies):
             return -1
     print("在第",pages,"页找到课程，开始选课")
     return pages
-
 
 #注销
 def logout(cookies):
@@ -205,10 +197,10 @@ def logout(cookies):
     logout = requests.post('http://10.3.255.178/logout.do',headers = headers , cookies = cookies, data = data)
     print("注销成功！")
 
-#主函数
-def xktest():
+def trylogin():
     loopcount = 1
     jsessionid = '0'
+    print("登陆信息",dataheader)
     while(jsessionid=='0'):
         print("尝试第",loopcount,"次登陆")
         try:
@@ -220,20 +212,11 @@ def xktest():
             print("登陆失败，请检查登陆信息和网络连接状态")
             return '0'
         time.sleep(1)
-
     cookies = {"JSESSIONID":jsessionid}
     print("登陆成功，准备进行选课")
+    return cookies
 
-    kcid = []
-    kcty = []
-    kcid = input("请输入课程id_课程序号 例：3412110160_01\n")
-    kcty = input("请输入课程类型，方案课程（2）、系任选课（4）、校任选课（3）\n")
-    maxloop = int(input("请输入最大抢课次数\n"))
-    sleeptime = float(input("请输入两次抢课的间隔（s）\n"))
-    input("按任意键开始选课")
-    print("开始寻找课程")
-
-    #跳转到选课方案界面
+def tryjump(cookies):
     chk = 0
     loopcount = 1
     select = []
@@ -250,24 +233,68 @@ def xktest():
                 return 0
         time.sleep(0.1)
     print("跳转成功！")
-    
-    #跳转到课程信息界面
-    pages = search(kcid,kcty,cookies)
-    if(pages == '-1'):
-        print("未找到课程")
-        return
-    print("在第",pages,"页找到课程，开始选课")
 
-    #选课
+def trycoursedata(kcid,kcty,cookies):
     chk = 0
-    while(chk != 1):
-        attempt(kcid,kcty,pages,cookies,maxloop,sleeptime)
-        if(chk == 0):
-            select = input("选课失败是否重试？y/n")
-            if(select == 'n'):
-                print("选课失败，准备注销")
-                logout(cookies)
-                return
+    loopcount = 1
+    select = []
+    while(chk == 0):
+        print("尝试第",loopcount,"次跳转至课程信息页面,课程类型(方案课程（2）、系任选课（4）、校任选课（3）)：",kcty)
+        chk = coursedata(kcid,kcty,cookies)
+        loopcount = loopcount + 1
+        if(loopcount == 1000):
+            print("跳转失败，网络错误或服务器压力过大")
+            select = input("是否重试？y/n: ")
+            if(select == 'y'):
+                loopcount = 1
+            else:
+                return 0
+        time.sleep(0.1)
+    print("跳转成功！")
+
+#主函数
+def xktest():
+    cookies = trylogin()
+    #kcid = input("请输入课程id_课程序号 例：3412110160_01\n")
+    #kcty = input("请输入课程类型，方案课程（2）、系任选课（4）、校任选课（3）\n")
+    #maxloop = int(input("请输入最大抢课次数\n"))
+    #sleeptime = float(input("请输入两次抢课的间隔（s）\n"))
+    print("课程id",kcid,"课程类别",kcty,"最大尝试次数",maxloop)
+    print("(方案课程（2）、系任选课（4）、校任选课（3）)")
+    input("按任意键开始选课")    
+    
+    #跳转到选课方案界面
+    tryjump(cookies)
+    prevty = "-1"
+    #选课
+    remain = len(kcid)
+    loopcount = 0
+    while(remain!=0 and (loopcount != maxloop or maxloop==0)):
+        chk = 0
+        loopcount = loopcount+1
+        print("第",loopcount,"/",maxloop,"次尝试,已选",len(kcid)-remain,"门,剩余",remain,"门",time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+        for i in range(len(kcid)):
+            if(success[i]==0):
+                if(prevty != kcty[i]):
+                    #跳转至课程信息页面
+                    trycoursedata(kcid[i],kcty[i],cookies)
+                    prevty = kcty[i]
+                chk = xk(kcid[i],kcty[i],"1",cookies)
+            if(chk==1):
+                print("成功选取：",kcid[i])
+                success[i]==1
+                remain = remain-1
+            
+            elif chk==-1:   #检测到掉线
+                print("检测到掉线，尝试重新登陆")
+                cookies = trylogin()#重新登陆
+                tryjump(cookies)    #跳转至选课方案页面
+                prevty = "-1"       #重置选课信息页面
+        if(loopcount==maxloop):
+            sel = input("达到最大尝试次数，是否继续？y/n")
+            if(sel == 'y'):
+                loopcount = 0
+        time.sleep(sleeptime)
     print("选课结束，准备注销")
     logout(cookies)
     return 0
